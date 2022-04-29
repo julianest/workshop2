a973a586e6fcdc80797c0724287aa5d1872669cb
julianest <julianest12@gmail.com>
2022-04-29 10:10:39 -0500

    [Add] Se añade enrutamiento

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,12 +1,36 @@
-import './App.css';
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
+import React, { useContext } from 'react';
+import { AppContext } from '../hooks/AppContext';
+import Home from '../components/Home';
+import Checkout from '../components/Checkout';
+import Nabvar from '../components/Navbar';
+
 
 function AppRoutes() {
+
+  const {product, setProduct}=useContext(AppRoutes)
+  const isEmpty = Object.keys(product).length;
+
   return (
-    <div className="App">
-      
-      
-      
-    </div>
+      <>
+        <div className='container'>
+            {isEmpty>0 ? (
+            <AppContext.Provider value={{product, setProduct}}>
+              <Nabvar/>
+                <BrowserRouter>
+                    <Routes>
+                    <Route path='/' element={<Home/>} />  
+                    <Route path='/checkout' element={<Checkout/>} />
+                    <Route path='/*' element={<Navigate to="/" />} />
+                  </Routes>
+                </BrowserRouter>  
+              </AppContext.Provider>) 
+              : <div class="spinner-border" role="status">
+              <span class="visually-hidden">Loading...</span>
+            </div>
+              }
+        </div>
+      </>
   );
 }
 
